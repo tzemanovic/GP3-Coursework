2304cddae707c7dc2dd5fc5deafc4cf87ec69f9d
Tomas Zemanovic <tzemanovic@gmail.com>
2014-12-06 22:45:29 +0000

    openGL initialization

--- GP3/src/openGlConfig.h
+++ GP3/src/openGlConfig.h
@@ -4,10 +4,10 @@
 struct OpenGlConfig
 {
 public:
-	OpenGlConfig( ) : antialiasingLevel( 16 ), depthBits( 0 ), stencilBits( 0 )
+	OpenGlConfig( ) : antialiasingLevel( 16 ), depthBits( 0 ), stencilBits( 0 ), majorVersion( 0 ), minorVersion( 0 )
 	{
 	}
-	OpenGlConfig( unsigned antialiasingLevel ) : antialiasingLevel( antialiasingLevel ), depthBits( 0 ), stencilBits( 0 )
+	OpenGlConfig( unsigned antialiasingLevel ) : antialiasingLevel( antialiasingLevel ), depthBits( 0 ), stencilBits( 0 ), majorVersion( 0 ), minorVersion( 0 )
 	{
 	}
 public:
